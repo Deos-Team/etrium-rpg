93ce5abbb940560ce02bfe73b704513016da9fca
Galiev <ggvalion@gmail.com>
2023-07-10 20:10:06 +0500

    Base configuration

---
+++ src/main/kotlin/dev/deos/entrium/Entrium.kt
@@ -0,0 +1,13 @@
+package dev.deos.entrium
+
+import com.mojang.logging.LogUtils
+import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
+
+object Entrium: ModInitializer {
+    const val MOD_ID = "entrium"
+    val logger: Logger = LogUtils.getLogger()
+    override fun onInitialize() {
+
+    }
+}
